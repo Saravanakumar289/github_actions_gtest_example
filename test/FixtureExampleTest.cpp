4be08c163da6095496f61e2287af7e3a552469bb
cvk1cob <k.saravanakumar@in.bosch.com>
2023-07-27 11:20:22 +0530

    Fixture

---
+++ test/FixtureExampleTest.cpp
@@ -0,0 +1,47 @@
+#include "gtest/gtest.h"
+class C{};
+
+class B{
+    C* ptr;
+    public:
+    B(C* addressOfObject): ptr{addressOfObject}{
+        
+    }
+};
+
+class A{
+    B* ptr;
+    public:
+    A(B* addressOfObject): ptr{addressOfObject}{
+
+    }
+    bool operation(){
+        return true;
+    }
+};
+
+TEST(TestFixtureTestsuite, OperationTrueTest){
+
+    // Arrange
+    C cObj;
+    B bObj(&cObj);
+    A aObj(&bObj);
+
+    // Act and Assert
+    ASSERT_TRUE(obj.operation());
+
+
+}
+
+TEST(TestFixtureTestsuite, OperationFalseTest){
+
+    // Arrange
+    C cObj;
+    B bObj(&cObj);
+    A aObj(&bObj);
+
+    // Act and Assert
+    ASSERT_FALSE(obj.operation());
+
+
+}
