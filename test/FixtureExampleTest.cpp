70d8b1e03ba571a1ddbe0c2e639c109553d2ebfd
cvk1cob <k.saravanakumar@in.bosch.com>
2023-07-27 11:46:19 +0530

    Fixture_F

--- test/FixtureExampleTest.cpp
+++ test/FixtureExampleTest.cpp
@@ -20,28 +20,41 @@ class A{
     }
 };
 
-TEST(TestFixtureTestsuite, OperationTrueTest){
-
-    // Arrange
-    C cObj;
-    B bObj(&cObj);
-    A aObj(&bObj);
+TEST_F(TestFixtureTestsuite, OperationTrueTest){
 
     // Act and Assert
-    ASSERT_TRUE(aObj.operation());
-
-
+    ASSERT_TRUE(aPtr->operation());
 }
 
-TEST(TestFixtureTestsuite, OperationFalseTest){
-
-    // Arrange
-    C cObj;
-    B bObj(&cObj);
-    A aObj(&bObj);
-
+TEST_F(TestFixtureTestsuite, OperationFalseTest){
     // Act and Assert
-    ASSERT_FALSE(aObj.operation());
+    ASSERT_FALSE(aPtr->operation());
+}
 
+class TestFixtureTestsuite:public testing::Test{
+    protected:
+    A* aPtr;
+    B* bPtr;
+    C* cPtr;
+    TestFixtureTestsuite(){
+        // Initialization
+    }
 
+    ~TestFixtureTestsuite(){
+        // Releasing the resources
+    }
+
+void setup(){
+    // Arrange
+    this->cPtr = new C();
+    this->bPtr = new B(cPtr);
+    this->aPtr = new A(bPtr)
+}
+
+void teardown(){
+    // delete resources
+    delete cPtr;
+    delete bPtr;
+    delete aPtr;
+}
 }
